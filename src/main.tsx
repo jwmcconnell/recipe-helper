6c58d1b1f5082ebd2a9c2134a9e744df6a9c8d01
Jack McConnell <jackmc1122@gmail.com>
2024-08-10 19:15:43 -0400

    Nested route

--- src/main.tsx
+++ src/main.tsx
@@ -10,12 +10,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Root } from "./pages/Root/Root.tsx";
 import { ErrorPage } from "./pages/Error/Error.tsx";
+import { Home } from "./pages/Home/Home.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,12 +1,16 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import { ReactElement } from "react";
+import { Outlet } from "react-router-dom";
 
 export function Root(): ReactElement {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography>Recipe Helper</Typography>
-      </Toolbar>
-    </AppBar>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>Recipe Helper</Typography>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+    </Box>
   );
 }
