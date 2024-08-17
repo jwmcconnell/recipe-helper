0d06bf2573ab5cb8a183cd0d1a94255e39c42134
Jack McConnell <jackmc1122@gmail.com>
2024-08-17 15:11:39 -0400

    Display auth info

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,10 @@
 import { Box, Button } from "@mui/material";
 import { ReactElement } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
 
 export function Home(): ReactElement {
-  const { loginWithRedirect } = useAuth0();
   return (
     <Box>
-      <Button sx={{ m: 1 }} onClick={() => loginWithRedirect()}>
-        Login
-      </Button>
       <Button sx={{ m: 1 }} component={RouterLink} to="/new/recipe">
         Add Recipe
       </Button>
--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,8 +1,11 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import { ReactElement } from "react";
 import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export function Root(): ReactElement {
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
   return (
     <Box>
       <AppBar position="static">
@@ -10,6 +13,22 @@ export function Root(): ReactElement {
           <Typography variant="h1" sx={{ fontSize: 36 }}>
             Recipe Helper
           </Typography>
+
+          <Button sx={{ m: 1 }} onClick={() => loginWithRedirect()}>
+            Login
+          </Button>
+
+          <Button sx={{ m: 1 }} onClick={() => logout()}>
+            Log Out
+          </Button>
+
+          {isAuthenticated && user && (
+            <div>
+              <img src={user.picture} alt={user.name} />
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
       <Outlet />
